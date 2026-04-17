7ca429e70cfd47f444b94145d443442339e04d9a
Varushka ☆彡 <fedorova04_varya@mail.ru>
2026-04-17 17:12:51 +0300

    mb good 3lab

--- 1_3/src/sorting.f90
+++ 1_3/src/sorting.f90
@@ -4,9 +4,25 @@ module Sorting
    implicit none
    
 contains
+   ! Функция сравнения должностей через findloc
+   pure function PositionLess(pos1, pos2, positions_rank) result(res)
+      character(POSITION_LEN, kind=CH_), intent(in) :: pos1, pos2
+      character(POSITION_LEN, kind=CH_), intent(in) :: positions_rank(:)
+      logical :: res
+      integer :: rank1, rank2
+      
+      rank1 = findloc(positions_rank, trim(pos1), dim=1)
+      rank2 = findloc(positions_rank, trim(pos2), dim=1)
+      
+      if (rank1 == 0 .or. rank2 == 0) then
+         res = .false.
+      else
+         res = rank1 > rank2  ! Меняем, если ранг первого больше
+      end if
+   end function PositionLess
    
-   !сортировка чёт-нечет
-   pure subroutine Sort_employee_list(employees, positions_rank)
+   !сортировка чёт-нечет 
+   pure subroutine SortEmpl(employees, positions_rank)
       type(employee), intent(inout) :: employees(:)
       character(POSITION_LEN, kind=CH_), intent(in) :: positions_rank(:)
       
@@ -20,10 +36,10 @@ contains
       do while (.not. sorted)
          sorted = .true.
          
-         !чётная фаза
+         !четная фаза
          do j = 1, n-1, 2
-            if (Need_swap(employees(j), employees(j+1), positions_rank)) then
-               !обмен
+            if (PositionLess(employees(j)%position, employees(j+1)%position, positions_rank)) then
+               !обмен местами
                tmp = employees(j)
                employees(j) = employees(j+1)
                employees(j+1) = tmp
@@ -31,33 +47,18 @@ contains
             end if
          end do
          
-         !нечётная фаза 
+         !нечётная фаза
          do j = 2, n-1, 2
-            if (Need_swap(employees(j), employees(j+1), positions_rank)) then
-               !обмен
+            if (PositionLess(employees(j)%position, employees(j+1)%position, positions_rank)) then
+               !обмен местами
                tmp = employees(j)
                employees(j) = employees(j+1)
                employees(j+1) = tmp
                sorted = .false.
             end if
          end do
-         
       end do
       
-   end subroutine Sort_employee_list
-   
-   !проверка, нужно ли менять местами сотрудников
-   pure logical function Need_swap(emp1, emp2, positions_rank)
-      type(employee), intent(in) :: emp1, emp2
-      character(POSITION_LEN, kind=CH_), intent(in) :: positions_rank(:)
-      
-      integer :: rank1, rank2
-      
-      rank1 = findloc(positions_rank, trim(emp1%position), dim=1)
-      rank2 = findloc(positions_rank, trim(emp2%position), dim=1)
-      
-      !меняем, если ранг первого больше ранга второго 
-      Need_swap = rank1 > rank2
-   end function Need_swap
+   end subroutine SortEmpl
    
 end module Sorting
